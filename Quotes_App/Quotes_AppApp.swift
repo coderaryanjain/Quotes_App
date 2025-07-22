501ef69f2168e93c579f746009c8e7a76d19ec49
coderaryanjain <jainaryan312@gmail.com>
2025-07-22 11:10:34 +0530

    Initial Commit

---
+++ Quotes_App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes_App
+//
+//  Created by Aryan Jain on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
